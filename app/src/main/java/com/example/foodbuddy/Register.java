a5fbf75eb91529e34e57059b7edfaf577a455521
DaSernet <Daanseijnhaeve@gmail.com>
2021-01-08 14:46:57 +0100

    Declared variables

--- app/src/main/java/com/example/foodbuddy/Register.java
+++ app/src/main/java/com/example/foodbuddy/Register.java
@@ -3,12 +3,32 @@ package com.example.foodbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    EditText mFullName,mEmail,mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mRegisterBtn = findViewById(R.id.registerButton);
+        mLoginBtn = findViewById(R.id.loginText);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressbar = findViewById(R.id.progressBar);
     }
 }
