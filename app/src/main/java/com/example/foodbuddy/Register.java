68e4668f183e553b5cf5867b0bc580486a7dcaaa
DaSernet <Daanseijnhaeve@gmail.com>
2021-01-08 14:58:30 +0100

    Added firebase registration and form validation

--- app/src/main/java/com/example/foodbuddy/Register.java
+++ app/src/main/java/com/example/foodbuddy/Register.java
@@ -1,13 +1,21 @@
 package com.example.foodbuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
@@ -30,5 +38,42 @@ public class Register extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
         progressbar = findViewById(R.id.progressBar);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(password.length() < 8){
+                    mPassword.setError("Password must be at least 8 characters");
+                    return;
+                }
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email cannot be empty!");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password cannot be empty!");
+                    return;
+                }
+
+                progressbar.setVisibility(View.VISIBLE);
+
+                //firebase registration
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User registered.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        } else{
+                            Toast.makeText(Register.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+            }
+        });
     }
 }
