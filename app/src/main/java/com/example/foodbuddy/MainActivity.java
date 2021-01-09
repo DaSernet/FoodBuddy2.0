fe13b031d135f6d752c41fffbc88428b6edf7624
DaSernet <Daanseijnhaeve@gmail.com>
2021-01-09 15:38:52 +0100

    Added toolbar

--- app/src/main/java/com/example/foodbuddy/MainActivity.java
+++ app/src/main/java/com/example/foodbuddy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.foodbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void logout(View view){
