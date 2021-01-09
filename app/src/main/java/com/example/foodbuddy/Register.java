8e950d5f91814272b61c6a42b49223f5d2ca542b
DaSernet <Daanseijnhaeve@gmail.com>
2021-01-09 15:17:40 +0100

    Added firebase storage completely

--- app/src/main/java/com/example/foodbuddy/Register.java
+++ app/src/main/java/com/example/foodbuddy/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +15,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.perfmark.Tag;
 
 public class Register extends AppCompatActivity {
+    public static final String TAG = "RegisterUser";
     EditText mFullName,mEmail,mPassword,mPassword2;
     Button mRegisterButton;
     TextView mLoginButton;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,7 @@ public class Register extends AppCompatActivity {
         mLoginButton = findViewById(R.id.loginText);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
         //If user is already logged in send to main activity
@@ -89,8 +102,25 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "User registered", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
 
+                            //Get UID from current user
+                            userID = fAuth.getCurrentUser().getUid();
+                            //Make collection using the userID
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+
+                            //creating new user object
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fullName",fullName);
+                            user.put("email",email);
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG,"onSuccess: user profile is created for"+ userID);
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         } else{
                             Toast.makeText(Register.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.INVISIBLE);
