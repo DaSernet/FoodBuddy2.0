4698f074d630176d21de13971fcd80c29a1fa3dd
DaSernet <Daanseijnhaeve@gmail.com>
2021-01-11 14:42:56 +0100

    Changed profile page

--- app/src/main/java/com/example/foodbuddy/AddRecipe.java
+++ app/src/main/java/com/example/foodbuddy/AddRecipe.java
@@ -7,7 +7,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
